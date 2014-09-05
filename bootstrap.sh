e077b4fdea872715111c454e5b132afd38f8a4ca
Juan Natural <juan@naturalcomolavidamisma.com>
2014-09-05 14:31:31 -0300

    removed .vim file from bootrat.sh so that it does not get overwritten by git pull

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".gitconfig .yadr .zshrc .rtorrent.rc .ssh .vim .xmonad .tmux.conf .vimrc"        # list of files/folders to symlink in homedir
+files=".gitconfig .yadr .zshrc .rtorrent.rc .ssh .xmonad .tmux.conf .vimrc"        # list of files/folders to symlink in homedir
 
 ##########
 
